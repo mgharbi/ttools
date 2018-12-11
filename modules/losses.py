be6fb5a7b5d8b111fd4990dc291cb0c56e2c37a9
Michael Gharbi <gharbi@mit.edu>
2018-12-11 07:37:02 -0800

    adds psnr

--- modules/losses.py
+++ modules/losses.py
@@ -2,6 +2,14 @@
 import torch as th
 
 
+class PSNR(th.nn.Module):
+    def __init__(self):
+        super(PSNR, self).__init__()
+        self.mse = th.nn.MSELoss()
+    def forward(self, out, ref):
+        mse = self.mse(out, ref)
+        return -10*th.log10(mse)
+
 
 # import torch.nn as nn
 # import torch.nn.functional as F
