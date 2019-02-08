fixes kernel lookup gradient

modules/ops.py
@@ -12,13 +12,26 @@ class KernelLookup(nn.Module):
         self.c_in = c_in
         self.ksize = ksize
         self.nkernels = nkernels
-        self.weights = nn.Parameter(th.zeros(nkernels, c_in, ksize, ksize))
+        self.weights = nn.Parameter(th.ones(nkernels, c_in, ksize, ksize))
 
         self.init_weights()
 
     def init_weights(self):
         std = 1.0 / math.sqrt(self.ksize*self.ksize*self.c_in)
+        # self.weights.data[..., 1, 1] = 1.0
+        # th.nn.init.uniform_(self.weights, 0, 2)
         th.nn.init.normal_(self.weights, std=std)
+
+        # self.weights.data.fill_(0.0)
+        # self.weights.data.fill_(0.5)
+        # self.weights.data[:, :, (self.ksize-1)//2, (self.ksize-1)//2] = 1.0
+
+        # nrm = self.weights.detach().abs().sum(-1, keepdim=True).sum(-2, keepdim=True)
+        # self.weights.data /= nrm
+        # th.nn.init.constant_(self.weights[0, 0, 1, 1], 1.0)
+        # th.nn.init.constant_(self.weights[1, 0, 0, 1], 1.0)
+        # th.nn.init.constant_(self.weights[2, 0, 1, 0], 1.0)
+        # th.nn.init.constant_(self.weights[3, 0, 1, 2], 1.0)
 
     def __repr__(self):
         s = "KernelLookup(c_in={}, ksize={}, nkernels={})".format(
@@ -26,4 +39,8 @@ class KernelLookup(nn.Module):
         return s
 
     def forward(self, data, kernel_idx):
-        return F.KernelLookup.apply(data, kernel_idx, self.weights)
+        n, c, h, w = self.weights.shape
+        weights = self.weights
+        weights = weights.view(n, c, h*w)
+        weights = th.nn.functional.softmax(weights, dim=-1).view(n, c, h, w)
+        return F.KernelLookup.apply(data, kernel_idx, weights)