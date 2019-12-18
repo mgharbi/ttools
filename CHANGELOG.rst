2ea0f0ac4c54121fd95ea5c7928bae32dee870ee
Michael Gharbi <gharbi@mit.edu>
2019-12-18 12:51:08 -0800

    im scriptts

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,8 @@
+0.0.20
+------
+
+- re-enable image scripts
+
 0.0.19
 ------
 
--- ttools/__main__.py
+++ ttools/__main__.py
@@ -1,39 +1,39 @@
 """Executable entrypoints."""
 import os
 import argparse
-# import imageio
+import imageio
 
 import ttools.scaffold as scaffold
 
-# import skimage.transform as xform
+import skimage.transform as xform
 
 
-# def resize():
-#     parser = argparse.ArgumentParser(description="upsample an image")
-#     parser.add_argument("image")
-#     parser.add_argument("factor", type=float, help="upsampling factor")
-#     parser.add_argument("output")
-#     args = parser.parse_args()
-#
-#     im = imageio.imread(args.image)
-#     im = xform.rescale(im, args.factor, order=0)
-#     imageio.imsave(args.output, im)
-#
-#
-# def im2vid():
-#     parser = argparse.ArgumentParser(
-#         description="converts a series of image frames to a video.")
-#     parser.add_argument("images", nargs="+")
-#     parser.add_argument("output")
-#     args = parser.parse_args()
-#
-#     images = []
-#     print("loading", len(args.images), "images")
-#     for filename in sorted(args.images):
-#         images.append(imageio.imread(filename))
-#         print(".")
-#     print("saving video")
-#     imageio.mimsave(args.output, images)
+def resize():
+    parser = argparse.ArgumentParser(description="upsample an image")
+    parser.add_argument("image")
+    parser.add_argument("factor", type=float, help="upsampling factor")
+    parser.add_argument("output")
+    args = parser.parse_args()
+
+    im = imageio.imread(args.image)
+    im = xform.rescale(im, args.factor, order=0)
+    imageio.imsave(args.output, im)
+
+
+def im2vid():
+    parser = argparse.ArgumentParser(
+        description="converts a series of image frames to a video.")
+    parser.add_argument("images", nargs="+")
+    parser.add_argument("output")
+    args = parser.parse_args()
+
+    images = []
+    print("loading", len(args.images), "images")
+    for filename in sorted(args.images):
+        images.append(imageio.imread(filename))
+        print(".")
+    print("saving video")
+    imageio.mimsave(args.output, images)
 
 
 def new_project():
